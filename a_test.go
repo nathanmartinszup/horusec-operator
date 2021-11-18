58d197b034e34185d4e593150d0e587a2e6b3c76
horusec <horusec@zup.com.br>
2021-11-18 17:17:49 +0000

    versioning:release - [skip ci] automatic commit updating versioning files
    Signed-off-by: horusec <horusec@zup.com.br>

--- a_test.go
+++ a_test.go
@@ -8,7 +8,7 @@ import (
 
 func TestName(t *testing.T) {
 	os.Setenv(EnvPlatformVersion, "v1.0.0")
-	os.Setenv(EnvActualVersion, "v2.3.8")
+	os.Setenv(EnvActualVersion, "")
 	os.Setenv(EnvReleaseVersion, "v2.3.9")
 
 	err := UpdateVersioningFiles()

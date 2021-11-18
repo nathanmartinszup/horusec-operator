6663871220650c37d7b6a6e84cdb0f69869d3c65
horusec <horusec@zup.com.br>
2021-11-18 15:08:31 -0300

    test

--- a_test.go
+++ a_test.go
@@ -1,19 +1,1 @@
 package main
-<<<<<<< HEAD
-
-import (
-	"fmt"
-	"os"
-	"testing"
-)
-
-func TestName(t *testing.T) {
-	os.Setenv(EnvPlatformVersion, "v1.0.0")
-	os.Setenv(EnvActualVersion, "")
-	os.Setenv(EnvReleaseVersion, "v2.3.9")
-
-	err := UpdateVersioningFiles()
-	fmt.Printf(err.Error())
-}
-=======
->>>>>>> 64a43c0 (test)

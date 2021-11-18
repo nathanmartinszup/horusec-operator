62f9dadafc305f31aada063f48426ac956488de5
horusec <horusec@zup.com.br>
2021-11-18 14:54:20 -0300

    test

--- a_test.go
+++ a_test.go
@@ -1,16 +1,16 @@
 package main
 
 import (
-	"fmt"
-	"os"
 	"testing"
 )
 
 func TestName(t *testing.T) {
-	os.Setenv(EnvPlatformVersion, "v1.0.0")
-	os.Setenv(EnvActualVersion, "")
-	os.Setenv(EnvReleaseVersion, "v2.3.9")
+	//os.Setenv(EnvPlatformVersion, "v1.0.0")
+	//os.Setenv(EnvActualVersion, "v2.3.8")
+	//os.Setenv(EnvReleaseVersion, "v2.3.9")
+	//
+	//err := UpdateVersioningFiles()
+	//fmt.Printf(err.Error())
 
-	err := UpdateVersioningFiles()
-	fmt.Printf(err.Error())
+	CherryPick()
 }
